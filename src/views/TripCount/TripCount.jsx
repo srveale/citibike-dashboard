3d467d65e5073a94438a8125f71fbc9eed05b07a
Scott Veale <srveale42@gmail.com>
2019-04-11 15:02:03 -0600

    different years for trip counts

--- src/views/TripCount/TripCount.jsx
+++ src/views/TripCount/TripCount.jsx
@@ -5,11 +5,21 @@ import { fetchRecent, fetchRealtime, fetchHourlyAverage } from '../../api/api';
 
 class TripCount extends React.Component {
   state = {
+  	appURL: "https://datascience.jimkemper.site:8080/app",
   }
+
+  updateYear(appURL) {
+  	this.setState({ appURL });
+  } 
 
   render() {
     return (
-      <iframe src="https://datascience.jimkemper.site:8080/app" style={{"width": "100%", "height": "500px"}}></iframe>
+    	<div>
+	    	<a href="#" onClick={() => this.updateYear("https://datascience.jimkemper.site:8080/app2016")} class="active">2016  </a>
+	    	<a href="#" onClick={() => this.updateYear("https://datascience.jimkemper.site:8080/app2017")}>2017  </a>
+	    	<a href="#" onClick={() => this.updateYear("https://datascience.jimkemper.site:8080/app")}>2018</a>
+	      <iframe src={this.state.appURL} style={{"width": "100%", "height": "500px"}}></iframe>
+	     </div>
     )
   }
 }
