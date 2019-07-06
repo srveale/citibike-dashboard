41f9c5c61cd2b15e545492d750f98d08a0582f18
Scott Veale <srveale42@gmail.com>
2019-07-06 17:09:51 -0600

    dummy data

--- src/api/api.js
+++ src/api/api.js
@@ -13,33 +13,40 @@ export function fetchRealtime (stationIdQuery) {
 }
 
 export function fetchRecent (stationIdQuery, historyDuration) {
-	return axios.get(`
-		https://cors-anywhere.herokuapp.com/
-		http://162.246.156.121/api/recent-data?
-		${stationIdQuery ? `stationid=${String(stationIdQuery)}` : ""}&
-		${historyDuration ? `historyDuration=${String(historyDuration)}` : ""}
-		`)
-	.then(pastTwoHoursResponse => {
-		// Format data for use in the chart
-		let labels = pastTwoHoursResponse.data.map(log => moment(log.executionTime).add(-6, 'hours').format('h:mm'));
-		let series = [pastTwoHoursResponse.data.map(log => log.availableBikes)];
-		if (historyDuration === 12) {
-			series = [series[0].filter((point, i) => i % 3 === 0)];
-			labels = labels.map((label, i) => i % 5 === 0 ? label : "").filter((point, i) => i % 3 === 0);
+	return {
+		data: {
+			series: [],
+			labels: [],
 		}
-		if (historyDuration === 24) {
-			series = [series[0].filter((point, i) => i % 6 === 0)];
-			labels = labels.map((label, i) => i % 30 === 0 ? label : "").filter((point, i) => i % 6 === 0);
-		}
-		const pastTwoHours = {
-		  data: {
-		    series,
-		    labels
-		  },
-		  logs: pastTwoHoursResponse.data,
-		};
-		return pastTwoHours;
-	})
+	}
+
+	// return axios.get(`
+	// 	https://cors-anywhere.herokuapp.com/
+	// 	http://162.246.156.121/api/recent-data?
+	// 	${stationIdQuery ? `stationid=${String(stationIdQuery)}` : ""}&
+	// 	${historyDuration ? `historyDuration=${String(historyDuration)}` : ""}
+	// 	`)
+	// .then(pastTwoHoursResponse => {
+	// 	// Format data for use in the chart
+	// 	let labels = pastTwoHoursResponse.data.map(log => moment(log.executionTime).add(-6, 'hours').format('h:mm'));
+	// 	let series = [pastTwoHoursResponse.data.map(log => log.availableBikes)];
+	// 	if (historyDuration === 12) {
+	// 		series = [series[0].filter((point, i) => i % 3 === 0)];
+	// 		labels = labels.map((label, i) => i % 5 === 0 ? label : "").filter((point, i) => i % 3 === 0);
+	// 	}
+	// 	if (historyDuration === 24) {
+	// 		series = [series[0].filter((point, i) => i % 6 === 0)];
+	// 		labels = labels.map((label, i) => i % 30 === 0 ? label : "").filter((point, i) => i % 6 === 0);
+	// 	}
+	// 	const pastTwoHours = {
+	// 	  data: {
+	// 	    series,
+	// 	    labels
+	// 	  },
+	// 	  logs: pastTwoHoursResponse.data,
+	// 	};
+	// 	return pastTwoHours;
+	// })
 }
 
 export function fetchWeather () {
@@ -50,13 +57,46 @@ export function fetchWeather () {
 }
 
 export function fetchPredictions() {
-	return axios.get(`https://cors-anywhere.herokuapp.com/http://162.246.156.121/api/predictions`)
-	.then(predictionsResponse => {
-		return predictionsResponse.data;
-	})	
+	return [
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		   0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+		     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		       0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+		        0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		            0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+	// return axios.get(`https://cors-anywhere.herokuapp.com/http://162.246.156.121/api/predictions`)
+	// .then(predictionsResponse => {
+	// 	return predictionsResponse.data;
+	// })	
 }
 
 export function fetchHourlyAverage (stationIdQuery) {
+	return {
+		data: {
+			series: [],
+			labels: [
+			  "00:00","","",
+			  "03:00","","",
+			  "06:00","","",
+			  "09:00","","",
+			  "12:00","","",
+			  "15:00","","",
+			  "18:00","","",
+			  "21:00","","",
+			],
+		}
+	};
+
 	return axios.get(`https://cors-anywhere.herokuapp.com/http://162.246.156.121/api/hourly-averages?${stationIdQuery ? `stationid=${String(stationIdQuery)}` : ""}`)
 	.then(hourlyResult => {
 		const sortedHours = hourlyResult.data.sort((a, b) => a.hour - b.hour);
